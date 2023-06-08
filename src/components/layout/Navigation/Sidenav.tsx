5432847aef13e30d3ff244aef7b6f90542a09437
Jakub Konefał <konefaljakub@gmail.com>
2023-06-08 10:46:10 +0200

    Adjust sidenav width

--- src/components/layout/Navigation/Sidenav.tsx
+++ src/components/layout/Navigation/Sidenav.tsx
@@ -3,21 +3,7 @@ import styled, { css } from 'styled-components'
 
 import { NavigationContext } from 'contexts/NavigationContext'
 
-import Icon from 'components/shared/icon'
 import { Text } from 'components/shared/typography'
-
-import sprzedamLogo from 'assets/icons/sprzedamlogolatest.svg'
-
-import sprzedamLogo1 from 'assets/icons/logosSvg/logo1.svg'
-import sprzedamLogo1V2 from 'assets/icons/logosSvg/logo1V2.svg'
-import sprzedamLogo2 from 'assets/icons/logosSvg/logo2.svg'
-import sprzedamLogo2V2 from 'assets/icons/logosSvg/logo2V2.svg'
-import sprzedamLogo2V3 from 'assets/icons/logosSvg/logo2V3.svg'
-import sprzedamLogo2V4 from 'assets/icons/logosSvg/logo2V4.svg'
-import sprzedamLogo2V5 from 'assets/icons/logosSvg/logo2V5.svg'
-import sprzedamLogo3 from 'assets/icons/logosSvg/logo3.svg'
-
-import closeIcon from 'assets/icons/close-white.svg'
 
 import useLocation from 'hooks/useLocation'
 
@@ -43,10 +29,6 @@ const SidenavWrapper = styled.nav`
   background-color: #0202028a;
   color: ${({ theme }) => theme.colors.black};
   transform: translateX(100vw);
-  ${({ theme }) => theme.media.s.min} {
-    width: 400px;
-    transform: translateX(400px);
-  }
 `
 
 const Overlay = styled.div<{ isVisible: boolean }>`
@@ -71,29 +53,6 @@ const Overlay = styled.div<{ isVisible: boolean }>`
         transform: translateX(0);
       }
     `}
-`
-
-const NavHeading = styled.div`
-  /* display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  position: relative;
-  padding: 0px 10px 0px 20px;
-  border-bottom: 2px solid #80808070; */
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  position: relative;
-  padding: 50px 20px;
-  border-bottom: 2px solid #80808070;
-  background-color: rgb(52, 52, 52);
-`
-
-const Logo = styled.a`
-  display: block;
 `
 
 const Links = styled.div`
@@ -145,25 +104,10 @@ const InnerWrapper = styled.div`
   padding: 0 20px 20px 20px;
   width: 100%;
   height: 100%;
-`
 
-const CloseBtn = styled.button`
-  position: absolute;
-  top: 15px;
-  right: 15px;
-  background: transparent;
-  border: none;
-
-  cursor: pointer;
-`
-
-const Shader = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 1px;
-  box-shadow: 1px -7px 20px 30px #0a06066d;
+  ${({ theme }) => theme.media.sm.min} {
+    padding: 0 32px 20px 32px;
+  }
 `
 
 const Sidenav: React.FC = () => {
