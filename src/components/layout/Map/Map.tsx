563bcadf55db12a36dd7f7a6d486580e6f2dbfc0
Jakub Konefał <konefaljakub@gmail.com>
2023-09-08 20:34:05 +0200

    Add log

--- src/components/layout/Map/Map.tsx
+++ src/components/layout/Map/Map.tsx
@@ -65,6 +65,8 @@ type Props = {
 }
 
 const Map: React.FC<Props> = ({ pins }) => {
+  console.log(process.env.GOOGLE_MAPS_API_KEY!)
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY!,
   })
